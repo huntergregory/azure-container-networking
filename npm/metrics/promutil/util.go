e032f9b3387070f2818293d4b1867110c1e56922
Hunter Gregory <t-hugreg@microsoft.com>
2020-06-30 17:09:34 -0400

    redesigned GetValue and GetCountValue so that they don't use http calls

--- npm/metrics/promutil/util.go
+++ npm/metrics/promutil/util.go
@@ -1,15 +1,10 @@
 package promutil
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"regexp"
-	"strconv"
 	"testing"
 
-	"github.com/Azure/azure-container-networking/npm/metrics"
 	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
 )
 
 const delayAfterHTTPStart = 10
@@ -33,45 +28,28 @@ func NotifyIfErrors(t *testing.T, errors ...error) {
 	}
 }
 
-// GetValue is used for validation. It returns a gaugeMetric's value as shown in the HTML Prometheus endpoint.
+// GetValue is used for validation. It returns a gaugeMetric's value.
 func GetValue(gaugeMetric prometheus.Collector) (int, error) {
-	return getMetricValue(metrics.GetMetricName(gaugeMetric))
-}
-
-// GetCountValue is used for validation. It returns the number of times a summaryMetric has recorded an observation as shown in the HTML Prometheus endpoint.
-func GetCountValue(summaryMetric prometheus.Collector) (int, error) {
-	return getMetricValue(metrics.GetMetricName(summaryMetric) + "_count")
-}
-
-func getMetricValue(metricName string) (int, error) {
-	metrics.StartHTTP(delayAfterHTTPStart)
-	regex := regexp.MustCompile(metricName + " [0-9]+")
-	if regex == nil {
-		return 0, fmt.Errorf("Couldn't compile regular expression for metric: " + metricName)
-	}
-	text, err := getMetricsText()
+	dtoMetric, err := getDTOMetric(gaugeMetric)
 	if err != nil {
 		return 0, err
 	}
-	locations := regex.FindStringIndex(text)
-	if locations == nil {
-		return 0, fmt.Errorf("Couldn't find a match for metric: " + metricName)
-	}
-	start := locations[0]
-	end := locations[1]
-	value := text[start+len(metricName)+1 : end]
-	return strconv.Atoi(value)
+	return int(dtoMetric.Gauge.GetValue()), nil
 }
 
-func getMetricsText() (string, error) {
-	response, err := http.Get("http://localhost" + metrics.HTTPPort + metrics.MetricsPath)
+// GetCountValue is used for validation. It returns the number of times a summaryMetric has recorded an observation.
+func GetCountValue(summaryMetric prometheus.Collector) (int, error) {
+	dtoMetric, err := getDTOMetric(summaryMetric)
 	if err != nil {
-		return "", err
+		return 0, err
 	}
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return "", err
-	}
-	return string(body), nil
+	return int(dtoMetric.Summary.GetSampleCount()), nil
+}
+
+func getDTOMetric(collector prometheus.Collector) (*dto.Metric, error) {
+	channel := make(chan prometheus.Metric, 1)
+	collector.Collect(channel)
+	metric := &dto.Metric{}
+	err := (<-channel).Write(metric)
+	return metric, err
 }
